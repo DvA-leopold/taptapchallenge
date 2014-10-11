ff83c1d215b2abc828aadcf9e59d2c9bada0ca23
Vladislav <Leopold.dva@gmail.com>
2014-10-12 00:41:39 +0400

    configures program (initial commit)

---
+++ desktop/src/com/taptap/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.taptap.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.taptap.game.TapTap;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TapTap(), config);
+	}
+}
