1d62eaaa3de4a256dbdc3880431aae0b3025082b
DurnevV <leopold.dva@gmail.com>
2014-12-14 00:15:04 +0300

    minor refactoring

--- core/test/com/taptap/game/save/manager/StorageManagerTest.java
+++ core/test/com/taptap/game/save/manager/StorageManagerTest.java
@@ -4,8 +4,6 @@ import com.badlogic.gdx.Gdx;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.Map;
-import static org.junit.Assert.*;
 
 public class StorageManagerTest {
 
